9f061e9625b5a1f03e1d9f47c6ea4109786299b6
aelbanyard <ael.banyard@stud.kbw.ch>
2023-04-11 11:14:08 +0200

    added types, params, functions to NumberInput

--- src/components/CustomInputs/NumberInput.tsx
+++ src/components/CustomInputs/NumberInput.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import { NumberInputParams } from "@/types";
+import { toNumber, toOutString } from "@/utils/functions";
+import React, { useState } from "react";
 
-const NumberInput = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const NumberInput = ({ value, onChange, placeholder }: NumberInputParams) => {
+  const [empty, setEmpty] = useState(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let input = "";
+    if (e.target.value === "") {
+      setEmpty(true);
+      input = Math.PI.toString();
+    } else if (
+      e.target.value.length > 1 &&
+      e.target.value[0] === "0" &&
+      e.target.value[1] === "."
+    ) {
+      setEmpty(false);
+      input = e.target.value;
+    } else if (e.target.value.length > 1 && e.target.value[0] === "0") {
+      setEmpty(false);
+      input = e.target.value.slice(1);
+    } else {
+      setEmpty(false);
+      input = e.target.value;
+    }
+    onChange(toNumber(input));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={toOutString(value)}
+        onFocus={($event) => toNumber($event.target.value)}
+        onBlur={($event) => toOutString($event.target.value)}
+        onChange={handleChange}
+      />
+      <p>{placeholder}</p>
+    </div>
+  );
 };
 
 export default NumberInput;
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type NumberInputParams = {
+  value: number;
+  onChange: (value: number) => void;
+  placeholder: string;
+};
