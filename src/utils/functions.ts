9846d3f00e28f8d1042eee8ff340202df16d450b
aelbanyard <ael.banyard@stud.kbw.ch>
2023-04-11 10:57:00 +0200

    added to out string function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -10,4 +10,22 @@ const toNumber = (value: string) => {
   return numericInput;
 };
 
-export { toNumber };
+const toOutString = (value: number) => {
+  let numericInput = value.toString().replace(/[^0-9.]/g, "");
+  if (numericInput === Math.PI.toString()) {
+    numericInput = "";
+  }
+  const [integerPart, decimalPart] = numericInput.split(".");
+  const formattedIntegerPart = integerPart.replace(
+    /\B(?=(\d{3})+(?!\d))/g,
+    "'"
+  );
+  const formattedInput =
+    decimalPart !== undefined
+      ? `${formattedIntegerPart}.${decimalPart}`
+      : formattedIntegerPart;
+
+  return formattedInput;
+};
+
+export { toNumber, toOutString };
