dc037f9058bf15cc4e1ffa52ff472465f75cb538
aelbanyard <ael.banyard@stud.kbw.ch>
2023-04-11 10:05:13 +0200

    added routes to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,52 @@
+import Link from "next/link";
+import { FaHouseDamage, FaHouseUser } from "react-icons/fa";
+import { BsCalculatorFill, BsHouseFill } from "react-icons/bs";
+
 const Navigation = () => {
-    return (
+  let routes = [
+    {
+      name: "Startseite",
+      path: "/",
+      icon: <BsHouseFill />,
+    },
+    {
+      name: "Hausrat",
+      path: "/hausrat",
+      icon: <FaHouseUser />,
+    },
+    {
+      name: "Schaden",
+      path: "/hausrat/schaden",
+      icon: <FaHouseDamage />,
+    },
+    {
+      name: "Versicherungssumme",
+      path: "/hausrat/versicherungssumme",
+      icon: <BsCalculatorFill />,
+    },
+  ];
+  return (
+    <>
+      <nav>
         <div>
-            
+          <Link href="/">
+            <p>Logo</p>
+          </Link>
         </div>
-    );
+        <div>
+          {routes.map((route, index) => {
+            return (
+              <p key={index}>
+                <Link href={route.path}>
+                  {route.icon} {route.name}
+                </Link>
+              </p>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navigation;
