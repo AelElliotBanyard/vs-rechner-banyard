25ab1841375786a901f9e23c26eb3e6953c0dc48
aelbanyard <ael.banyard@stud.kbw.ch>
2023-04-11 09:54:45 +0200

    put head in seperate file

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Header = () => {
+  return (
+    <>
+      <Head>
+        <title>Hausratversicherung Rechner</title>
+        <meta name="description" content="banyard.tech" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+    </>
+  );
+};
+
+export default Header;
