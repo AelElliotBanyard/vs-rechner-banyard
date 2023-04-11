9d7c33f43202e638b35bdb5e05cf2a36c849ffb3
aelbanyard <ael.banyard@stud.kbw.ch>
2023-04-11 10:11:10 +0200

    added text for startpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-
 export default function Home() {
   return (
     <>
       <main>
-        <h1>Hello World</h1>
+        <div>
+          <div>
+            <h1>Wilkommen!</h1>
+            <p>
+              Dies ist der Versicherungsrechner erstellt von{" "}
+              <a href="https://github.com/AelElliotBanyard" target="_blank">
+                Ael Banyard
+              </a>
+              .
+            </p>
+            <p>
+              Er ist noch in der Entwicklung, aber Sie können schon jetzt
+              berechnen was ihre Hausratsversicherung im Falle von einem Schaden
+              auszahlen.
+            </p>
+            <p>
+              Diese Web-App wurde erstellt im Rahmen eines Pilot Projekts an der
+              <a href="https://www.bbw.ch/" target="_blank">
+                BBW
+              </a>
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
