7b9cddef6797c74c2e300b25964b779666845903
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-21 14:18:32 +0200

    add locales to nextjs config file

--- CODE/next.config.js
+++ CODE/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['de-DE', 'en-GB', 'fr-FR'],
+    defaultLocale: "de-DE",
+  },
 }
 
 module.exports = nextConfig
