a5e8b22485a9d3e3241df210c51cd9cda2d62135
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-21 09:33:22 +0200

    add leaf-pattern bg to project

--- CODE/tailwind.config.js
+++ CODE/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "leaf-pattern": "url('/images/leaf-pattern.png')",
+      }
+    },
   },
   plugins: [],
 };
