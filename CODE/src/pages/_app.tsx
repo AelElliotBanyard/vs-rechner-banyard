7dd5826f48129f2b888182ffc4a14cc9b6bda8f6
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-21 09:33:37 +0200

    add leaf-patter bg to welcome page

--- CODE/src/pages/_app.tsx
+++ CODE/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className="flex flex-col h-auto min-h-screen">
       <Header />
       <Component {...pageProps} />
     </div>
