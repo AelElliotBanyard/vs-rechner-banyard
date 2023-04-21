22cd5d560ae98bc3c9ac140001d58c3d5419a417
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-21 15:35:11 +0200

    change text from header to text from text.json

--- CODE/src/components/Header.tsx
+++ CODE/src/components/Header.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Navigation from "./Navigation";
+import content from "../assets/text.json";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const { locale } = useRouter();
+  let text = content.header.filter((p) => p.locale === locale)[0];
   return (
     <>
       <Head>
-        <title>Hausratversicherung Rechner</title>
+        <title>{text.title}</title>
         <meta name="description" content="banyard.tech" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.png" type="image/png" />
