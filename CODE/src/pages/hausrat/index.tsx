4d2c4bccdc496014ff7b63246c3f13ec1d4486ef
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-21 15:14:17 +0200

    change text from household to text from text.json

--- CODE/src/pages/hausrat/index.tsx
+++ CODE/src/pages/hausrat/index.tsx
@@ -1,75 +1,68 @@
+import { useRouter } from "next/router";
+import content from "../../assets/text.json";
+
 const HausratMain = () => {
+  const { locale } = useRouter();
+  let text = content.household.filter((p) => p.locale === locale)[0];
   return (
     <>
       <main className="main">
         <div className="hausrat">
           <div className="section">
-            <h1>Hausratversicherung</h1>
+            <h1>{text.title}</h1>
             <div className="explain">
-              <h2>Was ist eine Hausratversicherung?</h2>
+              <h2>{text.what.title}</h2>
               <p>
-                Eine Hausratversicherung{" "}
-                <b>versichert Schäden an Ihrem Hausrat</b>, die durch Feuer,
-                Diebstahl, Wasser oder Elementarereignisse entstanden sind. Als
-                Hausrat gelten <b>bewegliche Sachen</b> in der Wohnung oder im
-                Haus – inkl. Keller, Garage, Estrich, Garten oder Balkon.
+                {text.what.text.map((t, i) => {
+                  if (t.bold) {
+                    return <b key={i}>{t.string}</b>;
+                  } else {
+                    return t.string;
+                  }
+                })}
               </p>
-              <p>Folgende Gegenstände sind im Normalfall versichert:</p>
+              <p>{text.whatIsCovered.title}</p>
               <ul>
-                <li>Möbel</li>
-                <li>Elektrogeräte</li>
-                <li>Kleidung</li>
-                <li>Schmuck, Uhren und andere Wertsachen</li>
-                <li>Vorhänge, Vasen, Teppiche</li>
-                <li>Sportartikel wie Velos und Ski</li>
-                <li>weitere Einrichtungsgegenstände</li>
+                {text.whatIsCovered.list.map((l, i) => {
+                  return <li key={i}>{l}</li>;
+                })}
               </ul>
             </div>
             <div className="pay">
               <div className="does">
-                <h2>Welche Schäden zahlt die Hausratversicherung?</h2>
+                <h2>{text.pay.does.title}</h2>
                 <p>
-                  Abgedeckt sind Schäden am Hausrat durch{" "}
-                  <b>
-                    Feuer, Wasser, Elementarereignisse wie etwa Sturmtiefs und
-                    Einbruchdiebstahl.
-                  </b>{" "}
-                  Zusätzlich können Sie{" "}
-                  <b>
-                    Glasbruchschäden oder den einfachen Diebstahl ausserhalb der
-                    Wohnung
-                  </b>{" "}
-                  versichern. Auch <b>anderweitige Kosten und Umtriebe</b> als
-                  Folge eines Schadensfalls sind mitversichert. Dazu zählen etwa
-                  Kosten für den Ersatz der Schlösser, Hotelübernachtungen oder
-                  Aufräum-, Reinigungs- und Entsorgungsarbeiten.
+                  {text.pay.does.text.map((l, i) => {
+                    if (l.bold) {
+                      return <b key={i}>{l.string}</b>;
+                    } else {
+                      return l.string;
+                    }
+                  })}
                 </p>
               </div>
               <div className="doesnt">
-                <h2>Welche Schäden zahlt die Hausratversicherung nicht?</h2>
+                <h2>{text.pay.doesNot.title}</h2>
                 <p>
-                  Grundsätzlich gilt: Die{" "}
-                  <b>Deckung der Hausratversicherung variiert</b> je nach
-                  Police. Prüfen Sie deshalb die Ausschlüsse in den Allgemeinen
-                  Versicherungsbedingungen. In den meisten Fällen sind
-                  Ereignisse im Zusammenhang mit Krieg, inneren Unruhen und
-                  Atomkraft von der Versicherung ausgeschlossen. Auch Schäden,
-                  die Sie mit Absicht herbeiführen, sind von der
-                  Hausratversicherung nicht gedeckt. Für Schäden am Eigentum von
-                  Dritten brauchen Sie eine <b>Privathaftpflichtversicherung</b>
-                  .
+                  {text.pay.doesNot.text.map((l, i) => {
+                    if (l.bold) {
+                      return <b key={i}>{l.string}</b>;
+                    } else {
+                      return l.string;
+                    }
+                  })}
                 </p>
               </div>
               <div className="you">
-                <h2>Was muss ich selbst zahlen?</h2>
+                <h2>{text.pay.you.title}</h2>
                 <p>
-                  Bei der Hausratversicherung reicht die Spanne des
-                  Selbstbehalts üblicherweise <b>von 200 bis 500 Franken</b>.
-                  Der Selbstbehalt wird von der Schadenssumme abgezogen. Ihn
-                  müssen Sie selbst bezahlen. Tipp: Achten Sie darauf, dass Sie
-                  den Selbstbehalt und die Deckungen an Ihre aktuellen
-                  Lebensumstände anpassen. So stellen Sie sicher, dass Sie nicht
-                  zu viel bezahlen oder unterversichert sind.
+                  {text.pay.you.text.map((l, i) => {
+                    if (l.bold) {
+                      return <b key={i}>{l.string}</b>;
+                    } else {
+                      return l.string;
+                    }
+                  })}
                 </p>
               </div>
             </div>
