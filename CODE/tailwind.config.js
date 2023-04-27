198cbabd9643fc6b190f44824e5c95d24eb855e7
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-27 14:44:58 +0200

    add custom animation

--- CODE/tailwind.config.js
+++ CODE/tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
         "bauhaus-fade": "url('/images/bauhaus-m-fade.png')",
         "triangles-fade": "url('/images/triangles-fade.png')",
         "circles-fade": "url('/images/circles-fade.png')",
-      }
+      },
+      animation: {
+        "spin-reverse": "spin 1s linear infinite reverse",
+      },
     },
   },
   plugins: [],
