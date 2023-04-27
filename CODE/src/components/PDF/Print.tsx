7774da08f37895813185a51e2a4b634c03cf52c9
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-27 14:19:36 +0200

    add content of print file

--- CODE/src/components/PDF/Print.tsx
+++ CODE/src/components/PDF/Print.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BlobProvider } from "@react-pdf/renderer";
+import { MdLoop, MdPrint } from "react-icons/md";
+import { PrintParams } from '@/types';
 
-const Print = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Print = ({ document, printable, check }: PrintParams) => {
+  return (
+    <BlobProvider document={document}>
+      {({ url, loading, error }) => (
+        <a
+          className="btn"
+          href={printable && !loading ? url || "#" : "#"}
+          target={printable && !loading ? "_blank" : "_self"}
+        >
+          {printable && !loading ? (
+            <MdPrint className="printIcon" />
+          ) : check != Math.PI ? (
+            <MdLoop className="loading" />
+          ) : (
+            <MdPrint className="printIcon" />
+          )}
+        </a>
+      )}
+    </BlobProvider>
+  );
 };
 
 export default Print;
