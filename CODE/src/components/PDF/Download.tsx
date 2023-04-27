bf95b76d0813ab4ed8cecc663e7b8d29dbf9bc68
aelbanyard <ael.banyard@lernende.bbw.ch>
2023-04-27 14:25:00 +0200

    add content of download file

--- CODE/src/components/PDF/Download.tsx
+++ CODE/src/components/PDF/Download.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BlobProvider } from "@react-pdf/renderer";
+import { MdDownload, MdLoop } from "react-icons/md";
+import { DownloadParams } from '@/types';
 
-const Download = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Download = ({ document, printable, filename, check }: DownloadParams) => {
+  return (
+    <BlobProvider document={document}>
+      {({ url, loading, error }) => (
+        <a
+          className="btn"
+          href={printable && !loading ? url || "#" : "#"}
+          target={printable && !loading ? "_blank" : "_self"}
+          download={filename}
+        >
+          {printable && !loading ? (
+            <MdDownload className="downloadIcon" />
+          ) : check != Math.PI ? (
+            <MdLoop className="loading" />
+          ) : (
+            <MdDownload className="downloadIcon" />
+          )}
+        </a>
+      )}
+    </BlobProvider>
+  );
 };
 
 export default Download;
